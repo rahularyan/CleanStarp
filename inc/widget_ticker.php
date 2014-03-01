d3042bcc60b035f051bdd750dae60bb34ba91e27
rahularyan <rah12@live.com>
2014-03-01 12:49:49 +0530

    Fixed ticker widget

--- inc/widget_ticker.php
+++ inc/widget_ticker.php
@@ -210,7 +210,7 @@
 		}
 		function output_widget($region, $place, $themeobject, $template, $request, $qa_content)
 		{
-			$widget_opt = $themeobject->current_widget['Ticker']['options'];
+			$widget_opt = $themeobject->current_widget['param']['options'];
 
 			$count = (isset($widget_opt['cs_ticker_count']) && !empty($widget_opt['cs_ticker_count'])) ?(int)$widget_opt['cs_ticker_count'] : 10;
 			

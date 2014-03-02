0d6da9a4f2c9a7c14d9d9b0b3cac66f2d6482443
Towhidn <Towhidn@gmail.com>
2014-03-02 01:30:08 -0800

    Category widget is fixed

--- inc/widget_categories.php
+++ inc/widget_categories.php
@@ -64,20 +64,29 @@
 			
 			return $allow;
 		}
-		function cs_category_navigation_sub($sub_categories,$depth){
+		function cs_category_navigation_sub($sub_categories,$depth,$path){
 			if ($depth<=1) return;
 			$output	= '';
 			if (is_array($sub_categories))
 				foreach ($sub_categories as $category){
+					$category_path = $path  . '/' . $category['tags'];
 					$output .= '<ul class="cs-category-widget-list-sub">';
-					$output .= '<li><a href="">' . $category['title'] . '</a>';
+					$output .= '<li><a href="' . qa_path_html('questions/'. $category_path) . '">' . $category['title'] . '</a>';
 					$sub_sub_categories = qa_db_select_with_pending(qa_db_category_sub_selectspec($category['categoryid']));
-					$output .= $this->cs_category_navigation_sub($sub_sub_categories,$depth-1);
+					$output .= $this->cs_category_navigation_sub($sub_sub_categories,$depth-1,$category_path);
 					$output .= '</li>';
 					$output .= '</ul>';
 				}
 
 			return $output;
+		}
+		function get_category_detail($title, $categories) {
+		   foreach ($categories as $key => $val) {
+			   if ($val['title'] === $title) {
+				   return $val;
+			   }
+		   }
+		   return null;
 		}
 		function output_widget($region, $place, $themeobject, $template, $request, $qa_content)
 		{
@@ -92,15 +101,16 @@
 			$categoryslugs=0;
 			$countslugs=0;
 			$themeobject->output('<div class="cs-category-widget">');
-			$categories = qa_category_navigation(qa_db_select_with_pending(qa_db_category_nav_selectspec($categoryslugs, false, false, true)));
+			$raw_categories = qa_db_select_with_pending(qa_db_category_nav_selectspec($categoryslugs, false, false, true));
+			$categories = qa_category_navigation($raw_categories);
 			unset($categories['all']);
 			$themeobject->output('<ul class="cs-category-widget-list">');
 			foreach ($categories as $category){
+				$category_detail = $this->get_category_detail($category['label'],$raw_categories);
 				//$sub_categories = qa_category_navigation(qa_db_select_with_pending(qa_db_category_nav_selectspec($categoryslugs, false, false, true)));
 				$sub_categories = qa_db_select_with_pending(qa_db_category_sub_selectspec($category['categoryid']));
-				
 				$themeobject->output('<li><a class="icon-folder-close-alt" href="' . $category['url'] . '">' . $category['label'] . '<span>'.filter_var($category['note'], FILTER_SANITIZE_NUMBER_INT).'</span></a>');
-				$themeobject->output($this->cs_category_navigation_sub($sub_categories,$depth));
+				$themeobject->output($this->cs_category_navigation_sub($sub_categories,$depth,$category_detail['tags']));
 				$themeobject->output('</li>');
 			}
 			$themeobject->output('</ul>');

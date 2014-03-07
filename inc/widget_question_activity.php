73265acae38887e399516bfa8de40dbbbfd1cda9
rahularyan <rah12@live.com>
2014-03-07 08:32:36 +0530

    Improved question activity widget

--- inc/widget_question_activity.php
+++ inc/widget_question_activity.php
@@ -68,7 +68,7 @@
 			$widget_opt = @$themeobject->current_widget['param']['options'];
 			$content = cs_get_cache_question_activity((int)$widget_opt['cs_qa_count']);
 			if(@$themeobject->current_widget['param']['locations']['show_title'])
-				$themeobject->output('<h3 class="widget-title">Recent Activity <a href="'.qa_path_html('activity').'">View All</a></h3>');
+				$themeobject->output('<h3 class="widget-title">'.qa_lang('cleanstrap/recent_activities').' <a href="'.qa_path_html('activity').'">'.qa_lang('cleanstrap/view_all').'</a></h3>');
 				
 			$themeobject->output('<div class="ra-question-activity-widget">');
 
@@ -76,7 +76,7 @@
 			
 			$themeobject->output('<ul class="activity-list">');
 			foreach ($q_list as $list){
-				$themeobject->output('<li class="clearfix"><span class="fav-star icon-heart'.(@$list['raw']['userfavoriteq'] ? ' active' : '').'"></span><span class="post-status-c">'.cs_post_status($list).'</span><a'.(is_featured($list['raw']['postid']) ? ' class="featured" ' : '').' href="'.$list['url'].'">'.$list['title'].'<span class="time">'.implode(' ', $list['when']).'</span><span class="ans-count total-'.$list['raw']['acount'].'">'.$list['raw']['acount'].'</span></a></li>');
+				$themeobject->output('<li class="clearfix '.(is_featured($list['raw']['postid']) ? ' featured' : '').'"><span class="fav-star icon-heart'.(@$list['raw']['userfavoriteq'] ? ' active' : '').'"></span><span class="post-status-c">'.cs_post_status($list).'</span><a href="'.$list['url'].'">'.$list['title'].'<span class="time">'.implode(' ', $list['when']).'</span><span class="ans-count total-'.$list['raw']['acount'].'">'.$list['raw']['acount'].'</span></a></li>');
 			}
 			$themeobject->output('</ul>');
 			$themeobject->output('</div>');

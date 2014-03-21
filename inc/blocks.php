b159b97fc806b8adad98c0ce0422f6b51a82019d
Towhidn <Towhidn@gmail.com>
2014-03-21 10:01:45 -0800

    nav issues with external files

--- inc/blocks.php
+++ inc/blocks.php
@@ -271,9 +271,11 @@ class qa_html_theme extends qa_html_theme_base
 				$this->content['navigation']['user']['admin'] = array('label' => qa_lang_html('cleanstrap/admin'), 'url' => qa_path_html('admin'), 'icon'=> 'icon-wrench');
 			$this->content['navigation']['user']['profile'] = array('label' => qa_lang_html('cleanstrap/profile'), 'url' => qa_path_html('user/' . qa_get_logged_in_handle()), 'icon'=> 'icon-vcard');
 			$this->content['navigation']['user']['updates']['icon'] = 'icon-signal';
-			$this->content['navigation']['user']['account'] = array('label' => qa_lang('cleanstrap/account'), 'url' => qa_path_html('account'), 'icon' => 'icon-cog');
+			if(!(QA_FINAL_EXTERNAL_USERS)) 
+				$this->content['navigation']['user']['account'] = array('label' => qa_lang('cleanstrap/account'), 'url' => qa_path_html('account'), 'icon' => 'icon-cog');
 			$this->content['navigation']['user']['favorites'] = array('label' => qa_lang('cleanstrap/favorites'), 'url' => qa_path_html('favorites'), 'icon' =>'icon-heart');
-			$this->content['navigation']['user']['wall'] = array('label' => qa_lang('cleanstrap/wall'), 'url' => qa_path_html('user/'.qa_get_logged_in_handle().'/wall'), 'icon' =>'icon-edit');
+			if(!(QA_FINAL_EXTERNAL_USERS))
+				$this->content['navigation']['user']['wall'] = array('label' => qa_lang('cleanstrap/wall'), 'url' => qa_path_html('user/'.qa_get_logged_in_handle().'/wall'), 'icon' =>'icon-edit');
 			$this->content['navigation']['user']['recent_activity'] = array('label' => qa_lang('cleanstrap/recent_activity'), 'url' => qa_path_html('user/'.qa_get_logged_in_handle().'/activity'), 'icon' =>'icon-time');
 			$this->content['navigation']['user']['all_questions'] = array('label' => qa_lang('cleanstrap/all_questions'), 'url' => qa_path_html('user/'.qa_get_logged_in_handle().'/questions'), 'icon' =>'icon-question');
 			$this->content['navigation']['user']['all_answers'] = array('label' => qa_lang('cleanstrap/all_answers'), 'url' => qa_path_html('user/'.qa_get_logged_in_handle().'/answers'), 'icon' =>'icon-answer');
@@ -286,11 +288,11 @@ class qa_html_theme extends qa_html_theme_base
                     echo '<li class="user-nav-'.$k . (isset($a['selected']) ? ' active' : '') . '"><a' . $icon . ' href="' . @$a['url'] . '" title="' . @$a['label'] . '">' . @$a['label'] . '</a></li>';
                 }
             }
-			
-           // if (isset($this->content['navigation']['user']['logout']['url'])) {
-                $link = qa_opt('site_url') . "logout";
+				if (isset($this->content['navigation']['user']['logout']['url']))
+					$link = $this->content['navigation']['user']['logout']['url'];
+				else
+					$link = qa_opt('site_url') . "logout";			
                 echo "<li><a class='icon-switch' href = '$link'> " . qa_lang_html('cleanstrap/logout') . " </a></li>";
-           // }
 ?>
 						</ul>
 					</li>
